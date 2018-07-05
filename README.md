2b231be7853198515afc44895ee16c426a4ce952
Joel Weirauch <joel@weirau.ch>
2018-07-05 17:57:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Work in progress golang utility to save combined bash history from all terminal sessions into a sqlite database
+
+NB: Make sure to save off a copy of your `~/.bash_history` before using this tool, otherwise you will lose it!  Also, this is a very quick and rough utility that needs some more work but it should be stable as-is, though your mileage may vary.
+
+Place the `shist` binary somewhere in your path and then add the following to your `~/.bashrc`
+
+```
+prompt_command() {
+    history | sed '$!d' | cut -c 8- | shist && history -c && shist -fetch && history -r
+}
+
+PROMPT_COMMAND=prompt_command
+```
+
+Source your new `~/.bashrc` or reload your terminal session(s) and everything should work.
---
+++ shist.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+  "io"
+  "os"
+  "fmt"
+  "flag"
+  "bufio"
+  "strings"
+  "io/ioutil"
+  "database/sql"
+  "github.com/op/go-logging"
+  _ "github.com/mattn/go-sqlite3"
+  "github.com/mitchellh/go-homedir"
+)
+
+var app_version = "0.1.0"
+var history_database, _ = homedir.Expand("~/.shist")
+var bash_history, _ = homedir.Expand("~/.bash_history")
+
+var log = logging.MustGetLogger("shist")
+
+var (
+	printHelp bool
+        version bool
+        fetch bool
+)
+
+func init_database(database *sql.DB) {
+    statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME DEFAULT CURRENT_TIMESTAMP)")
+    statement.Exec()
+}
+
+func main() {
+	log_backend, err := logging.NewSyslogBackend("shist")
+
+	if err != nil { fmt.Printf("Error: %", err) }
+
+	logging.SetBackend(log_backend)
+        var format = logging.MustStringFormatter("%{level} %{message}")
+        logging.SetFormatter(format)
+        logging.SetLevel(logging.INFO, "shist")
+
+	database, _ := sql.Open("sqlite3", history_database)
+
+	init_database(database)
+
+	flag.BoolVar(&printHelp, "help", false, "Print this help message.")
+        flag.BoolVar(&fetch, "fetch", false, "Save out the bash history file")
+        flag.BoolVar(&version, "version", false, "Show Version")
+
+        flag.Parse()
+
+	if printHelp {
+		fmt.Println("NAME:")
+		fmt.Println("    shist - A smart bash history utility\n")
+		fmt.Println("DESCRIPTION:")
+		fmt.Println("    A smart utility for saving and fetching bash command history.\n")
+		fmt.Println("VERSION:")
+		fmt.Println("    " + app_version + "\n")
+		fmt.Println("FLAGS:")
+		flag.PrintDefaults()
+		os.Exit(0)
+        }
+
+	if fetch {
+		rows, _ := database.Query("SELECT * FROM history ORDER BY created asc")
+
+		var id int
+		var entry string
+		var created string
+		var history string
+
+		defer rows.Close()
+		for rows.Next() {
+			rows.Scan(&id, &entry, &created)
+			history += entry + "\n"
+		}
+
+		os.Remove(bash_history)
+		err := ioutil.WriteFile(bash_history, []byte(history), 0644)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		os.Exit(0)
+	}
+
+        if version {
+                fmt.Println(app_version)
+                os.Exit(0)
+        }
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		input, err := reader.ReadString('\n')
+		if err != nil && err == io.EOF {
+			break
+		}
+		input = strings.TrimSpace(input)
+
+		rows, _ := database.Query("SELECT count(*) FROM history where entry = ?", input)
+
+		var count int
+
+		defer rows.Close()
+		for rows.Next() {
+			rows.Scan(&count)
+		}
+
+		if (count <= 0) {
+			statement, _ := database.Prepare("INSERT INTO history (entry) VALUES (?)")
+			_, err := statement.Exec(input)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+
